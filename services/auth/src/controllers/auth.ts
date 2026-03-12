59ba623b58dd1d21898fe0c507f2338ec2c78d50
dev <devesh712005@gmail.com>
2026-03-12 15:21:04 +0530

    feat(auth-service): implement user registration and login

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -3,6 +3,9 @@ import { TryCatch } from "../utils/TryCatch.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { sql } from "../utils/db.js";
 import bcrypt from "bcrypt";
+import getBuffer from "../utils/buffer.js";
+import axios from "axios";
+import jwt from "jsonwebtoken";
 
 export const registerUser = TryCatch(async (req, res, next) => {
   const { name, email, password, phoneNumber, role, bio } = req.body;
@@ -24,11 +27,68 @@ export const registerUser = TryCatch(async (req, res, next) => {
     registeredUser = user;
   } else if (role === "jobseeker") {
     const file = req.file;
+
+    if (!file) {
+      throw new ErrorHandler(400, "Resume file is required for jobseekers");
+    }
+    const fileBuffer = await getBuffer(file);
+    if (!fileBuffer || !fileBuffer.content) {
+      throw new ErrorHandler(500, "Failed to generate buffer");
+    }
+    const { data } = await axios.post(
+      `${process.env.UPLOAD_SERVICE}/api/utils/upload`,
+      { buffer: fileBuffer.content },
+    );
     const [user] =
-      await sql`INSERT INTO users (name,email,password,phone_number,role) VALUES 
-                            (${name},${email},${hashPassword},${phoneNumber},${role}) RETURNING 
-                            user_id,name,email,role,phone_number,role,created_at`;
+      await sql`INSERT INTO users (name,email,password,phone_number,role,bio,resume,resume_public_id) VALUES 
+                            (${name},${email},${hashPassword},${phoneNumber},${role},${bio},${data.url},${data.public_id}) RETURNING 
+                            user_id,name,email,role,phone_number,role,bio,resume,created_at`;
     registeredUser = user;
   }
-  res.json(email);
+  const token = jwt.sign(
+    { id: registeredUser?.user_id },
+    process.env.JWT_SEC as string,
+    {
+      expiresIn: "15d",
+    },
+  );
+  res.json({
+    message: "User Registered",
+    registeredUser,
+    token,
+  });
+});
+
+export const loginUser = TryCatch(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ErrorHandler(400, "Please fill all details");
+  }
+  const user = await sql`
+  SELECT u.user_id,u.name,u.email,u.password,u.phone_number,u.role,u.bio,u.resume,u.profile_pic,u.subscription, ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) as skills FROM users u LEFT JOIN user_skills us ON u.user_id = us.user_id 
+  LEFT JOIN skills s ON us.skill_id = s.skill_id
+  WHERE u.email = ${email} GROUP BY u.user_id;
+  `;
+  if (user.length === 0) {
+    throw new ErrorHandler(400, "Invalid Credentials");
+  }
+  const userObject = user[0];
+  const matchPassword = await bcrypt.compare(password, userObject.password);
+  if (!matchPassword) {
+    throw new ErrorHandler(400, "Invalid credentials");
+  }
+  userObject.skills = userObject.skills || [];
+  delete userObject.password;
+  const token = jwt.sign(
+    { id: userObject?.user_id },
+    process.env.JWT_SEC as string,
+    {
+      expiresIn: "15d",
+    },
+  );
+  res.json({
+    message: "User Loggedin",
+    userObject,
+    token,
+  });
 });
--- services/auth/src/utils/db.ts
+++ services/auth/src/utils/db.ts
@@ -1,4 +1,4 @@
 import { neon } from "@neondatabase/serverless";
 import dotenv from "dotenv";
 dotenv.config();
-export const sql = neon(process.env.DB_URl as string);
+export const sql = neon(process.env.DB_URL as string);
