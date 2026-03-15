7a064a13c03d4cd3de36864647244915ded59de1
dev <devesh712005@gmail.com>
2026-03-15 10:54:21 +0530

    feat(user-service): add functionality to add and delete user skills

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { isAuth } from "../middlewares/auth.js";
 import {
+  addSkillToUser,
+  deleteSkillFromUser,
   getUserProfile,
   myprofile,
   updateProfilePic,
@@ -14,4 +16,6 @@ router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
 router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
 router.put("/update/resume", isAuth, uploadFile, updateResume);
+router.post("/skill/add", isAuth, addSkillToUser);
+router.delete("/skill/delete", isAuth, deleteSkillFromUser);
 export default router;
